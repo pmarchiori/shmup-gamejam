541da28dd4698d109821f9269a9ae1cf474aa367
KarlKhoa <khoavokm@gmail.com>
2024-09-13 16:10:13 +0800

    player and boss death anim

--- shmup-gamejam/Assets/Scripts/Ships/Player.cs
+++ shmup-gamejam/Assets/Scripts/Ships/Player.cs
@@ -4,9 +4,12 @@ namespace Shmup
 {
     public class Player : Ship
     {
+        public Animator anim;
+
         protected override void Die()
         {
             Debug.Log("Player dead");
+            anim.Play("player_death", 0, 0.25f);
             Destroy(gameObject);
         }
     }
